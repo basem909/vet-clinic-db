97e0c55046cd8ae0f5e9d28d4250359fa3968f78
bassem <basemshams30@gmail.com>
2022-07-15 04:21:47 -0300

    Update queries.sql file

--- queries.sql
+++ queries.sql
@@ -16,4 +16,25 @@ SELECT neutered, AVG(escape_attempts) AS "Who escaped the most?" FROM animals GR
 SELECT species, MIN(weight_kg) AS "minimum weight", MAX(weight_kg) AS "maximum weight" FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species; 
 
+SELECT name, full_name FROM owners
+ JOIN animals ON owners.id = animals.owner_id WHERE full_name = 'Melody Pond';
+
+SELECT animals.name, species.name AS animal_type FROM animals
+ JOIN species ON species_id = species.id WHERE species.name = 'Pokemon';
+
+SELECT full_name, name AS animal_name FROM owners
+ LEFT JOIN animals ON owners.id = animals.owner_id;
+ SELECT COUNT(animals.name), species.name FROM animals
+ JOIN species ON species_id=species.id GROUP BY species.name;
+ 
+SELECT animals.name AS animal_name, full_name AS owner, species.name AS type FROM animals
+ JOIN owners ON owner_id=owners.id
+ JOIN species ON species_id=species.idWHERE full_name='Jennifer Orwell' AND species.name='Digimon';
+   
+SELECT animals.name AS animal_name, escape_attempts, full_name AS owner FROM animals
+ JOIN owners ON owner_id=owners.id WHERE full_name='Dean Winchester' AND escape_attempts = 0;
+   
+SELECT owners.full_name as owner, count(animals.id) FROM animals
+ JOIN owners ON (animals.owner_id = owners.id) GROUP BY owners.full_name ORDER BY count(animals.id) DESC LIMIT 1;
+
 SELECT * FROM animals;
