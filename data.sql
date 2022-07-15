5926e828c83c087d8e145a1f38d04b12bf8845cd
bassem <basemshams30@gmail.com>
2022-07-14 23:51:05 -0300

    Update data and queries files

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)  overriding system value values (1, 'Agumon', '03-02-2020',0, true, 10.23);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)  overriding system value values (2, 'Gabumon', '2018-11-15',2, true, 8.00);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)  overriding system value values (3, 'Pikachu', '07-01-2021',1, false, 15.04);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)  overriding system value values (4, 'Agumon', '12-05-2017',5, true, 10.23);
