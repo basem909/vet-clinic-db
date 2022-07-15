5a2f3ec22027d6857be2ed49276e094ff26100d8
bassem <basemshams30@gmail.com>
2022-07-15 03:54:45 -0300

    Update data.sql file

--- data.sql
+++ data.sql
@@ -11,3 +11,44 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)  overriding system value values (9, 'Boarmon', '07-06-2005',7, true, 20.4);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)  overriding system value values (10, 'Blossom', '13-10-1998',3, true, 17.00);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)  overriding system value values (10, 'BDitto', '14-05-2022',4, true, 22.00);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+UPDATE animals
+ SET species_id=(SELECT id FROM species WHERE name='Digimon')
+  WHERE name LIKE '%mon'
+;
+
+UPDATE animals
+SET species_id=(SELECT id FROM species WHERE name='Pokemon')
+WHERE species_id IS NULL
+;
+
+UPDATE animals
+SET owner_id=(SELECT id FROM owners WHERE full_name ='Dean Winchester')
+WHERE name IN ('Angemon', 'Boarmon');
+
+UPDATE animals
+SET owner_id=(SELECT id FROM owners WHERE full_name ='Bob')
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id=(SELECT id FROM owners WHERE full_name ='Melody Pond')
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id=(SELECT id FROM owners WHERE full_name ='Sam Smith')
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id=(SELECT id FROM owners WHERE full_name ='Jennifer Orwell')
+WHERE name IN ('Gabumon', 'Pikachu');
