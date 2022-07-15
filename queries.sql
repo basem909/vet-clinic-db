daa2ca0c7bd39776d39c68ade9ac848729a20b1b
bassem <basemshams30@gmail.com>
2022-07-15 02:04:44 -0300

    Update queries.sql file

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-ELECT * FROM animals WHERE name like '%mon';
+SELECT * FROM animals WHERE name like '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT * FROM animals WHERE neutered = true and escape_attempts < 3;
 SELECT name, date_of_birth FROM animals WHERE name = 'Agumon' or name = 'Pikachu';
@@ -8,4 +8,11 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg < 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts= 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, SUM(escape_attempts) AS "Who escaped the most?" FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg) AS "minimum weight", MAX(weight_kg) AS "maximum weight" FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species; 
+
 SELECT * FROM animals;
