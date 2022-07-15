b82b9691b9eb4e91d049a381f86e1486517ea8fe
bassem <basemshams30@gmail.com>
2022-07-15 00:31:18 -0300

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -9,3 +9,27 @@ id INT Generated always as Identity,
 	weight_kg Decimal,
 	primary KEY (id)
 );
+ALTER TABLE animals
+ADD species VARCHAR(50);
+BEGIN TRANSACTION;
+UPDATE animals
+SET species= 'unspecified';
+ROLLBACK;
+BEGIN TRANSACTION;
+UPDATE animals
+SET species= 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species= 'pokemon'
+WHERE species ISNULL;
+COMMIT TRANSACTION;
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg= weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg= weight_kg* -1 WHERE weight_kg < 0;
+COMMIT TRANSACTION;
